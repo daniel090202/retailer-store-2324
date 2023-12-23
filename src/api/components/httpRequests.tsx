8a3c3dd837c03f10264e721c55dc82933ca43479
Daniel Nguyen <minhkhanh090202@gmail.com>
2023-12-23 21:16:11 +0700

    set-up-project-structure

---
+++ src/api/components/httpRequests.tsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const request = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
+
+const get = async (url: string, options: object = {}) => {
+  const response = await request.get(url, options);
+
+  return response.data;
+};
+
+export { get };
+export default request;
