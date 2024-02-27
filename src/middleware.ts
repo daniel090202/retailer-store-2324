1f757ff53fb6dd55445f57c5b26c46c538cedf04
Daniel Nguyen <minhkhanh090202@gmail.com>
2024-02-28 00:56:14 +0700

    adjust-next-auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,8 @@
-// import { getSession } from "next-auth/react";
 import { withAuth } from "next-auth/middleware";
-
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
 const config = { matcher: ["/:path*"] };
 
 export default withAuth({
-  callbacks: {
-    authorized: ({ token }) => {
-      if (token?.accessToken) {
-        return true;
-      }
-
-      return false;
-    },
-  },
   pages: {
     error: "/auth/error",
     signIn: "/auth/login",
