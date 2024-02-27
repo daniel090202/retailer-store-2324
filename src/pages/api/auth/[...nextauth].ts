a76dee19d77308be5b69a4e372fb624f63e40091
Daniel Nguyen <minhkhanh090202@gmail.com>
2024-02-28 00:03:15 +0700

    adjust-next-auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -50,6 +50,7 @@ const authOptions: NextAuthOptions = {
         } else {
           const errorMessage = "Account credentials failed.";
 
+          console.log(userData);
           throw new Error(userData.message);
         }
       },
